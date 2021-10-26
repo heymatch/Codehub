7de2672e7f91a5a699b94fd4cdee9eb0a0583981
heymatch <match.remo@gmail.com>
2021-10-26 22:15:44 +0800

    CodeForces Practice

---
+++ competitive/CodeForces/ProblemSet/1331D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int w; cin >> hex >> w;
+
+    cout << w % 2 << endl; 
+
+    return 0;
+}
---
+++ competitive/CodeForces/ProblemSet/331C2.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    unsigned long long n; cin >> n;
+
+    unsigned long long ans = 0;
+    while(n){
+        
+    }
+
+    return 0;
+}
